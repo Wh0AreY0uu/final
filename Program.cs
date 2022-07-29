d6ee94ebebc6043bf2b3516992ad299a4ec83266
Yuriy Isabaev <isabaevyy@gmail.com>
2022-07-29 20:05:10 +0300

    update program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr = new string[5] { "moby", "78", "hi", "try", "cry" };
+string[] arr2 = new string[arr.Length];
+int index = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        arr2[index] = arr[i];
+        index++;
+    }
+}
+Console.Write("[ ");
+
+for (int i = 0; i < arr2.Length; i++)
+{
+    Console.Write($"{arr2[i]} ");
+
+}
+Console.Write("]");
+
